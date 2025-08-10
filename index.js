ca804bd4faf9b4b1de158564316b24aa6b26ad6a
Alvert <alvertfabianaponte26@gmail.com>
2025-08-10 00:46:21 -0300

    pito

--- index.js
+++ index.js
@@ -7,6 +7,20 @@ const imagenarda = document.getElementById("imagenBola");
 const subtitulo = document.getElementById("mensaje");
 const sonido = document.getElementById("miSonido");
 const puntardos = document.getElementById("puntaje");
+
+const iconoMankey = document.createElement("img");
+iconoMankey.classList.add("claseMono");
+
+const iconoPika = document.createElement("img");
+iconoPika.classList.add("clasePika");
+
+const iconoJiggly = document.createElement("img");
+iconoJiggly.classList.add("clasePuff");
+
+
+const iconoEstrella = document.createElement("img");
+iconoEstrella.classList.add("estrellita");
+
 let valorObjetoDeRepuesta  ;
 
 
@@ -70,15 +84,19 @@ const array = [
 
 
 botonJuego.addEventListener("click", () => {
+    
      subtitulo.style.textShadow ="5px 4px 3px rgba(100, 100, 100, 1)"
     subtitulo.style.color = "#000"; 
     imagenarda.style.filter = "brightness(0) ";
-   
-    
+    subtitulo.classList.remove("superclase");
+    mensaje.classList.remove("mensaje");
+    mensaje.style.fontSize = "";
+    mensaje.style.paddingRight = "";
+    subtitulo.style.textShadow ="";
     
     entrada.value = "";
     
-fetch(`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random()*60)}`)
+fetch(`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random()*100)}`)
 .then(respuesta => respuesta.json())
 .then((objeto) => {
  
@@ -118,19 +136,62 @@ if(entrada.value.toLowerCase().trim() === valorObjetoDeRepuesta.name){
     sonido.play();
     imagenarda.style.filter = "none";
     subtitulo.innerText = `¡Felicidades es ${valorObjetoDeRepuesta.name}!`;
-    subtitulo.style.color = "rgba(74, 190, 7, 1)"
-    subtitulo.style.textShadow ="6px 4px 1px rgba(0, 0, 0, 1)";
+    subtitulo.style.color = "rgba(87, 201, 21, 1)"
+    subtitulo.style.textShadow ="6px 4px 0px rgba(0, 0, 0, 1)";
     sumaPuntos += 1;
+    puntaje.innerText = `Puntos: ${sumaPuntos } pts`;   }
     
     
+ 
+switch(sumaPuntos){
+    case 5:
+        subtitulo.innerText = "5 puntos? nada mal.."
+        subtitulo.style.color = "rgba(199, 70, 10, 1)"
+        subtitulo.classList.add("superclase");
+       subtitulo.style.textShadow ="5px 4px 0px rgba(0, 0, 0, 1)";
+       iconoMankey.src = "mankey.png";
+       mensaje.style.fontSize = "40px";
+       subtitulo.appendChild(iconoMankey);
+        
+    break;
+
+    case 10:
+        subtitulo.innerText = "¡VAYA!si que sabes de pokemons"
+        subtitulo.style.color = "rgba(218, 96, 163, 1)"
+        subtitulo.classList.add("superclase");
+        iconoJiggly.src = "jiggly.png";
+       subtitulo.appendChild(iconoJiggly);
+       mensaje.style.paddingRight = "20%";
+       subtitulo.style.textShadow ="5px 4px 0px rgba(0, 0, 0, 1)";
+       break;
+      
+
+    case 15:
+        subtitulo.innerText = "15pts? sabes mucho de pokemons"
+        subtitulo.style.color = "rgba(218, 126, 7, 1)"
+        subtitulo.classList.add("superclase");
+        iconoPika.src = "pika.png";
+        subtitulo.appendChild(iconoPika);
+        mensaje.style.paddingRight = "20%";
+        mensaje.style.fontSize = "32px";
+        subtitulo.style.textShadow ="5px 4px 0px rgba(0, 0, 0, 1)";
+    break;
+
+
+    case 20:
+       subtitulo.style.color ="rgba(245, 181, 6, 1) "; 
+       subtitulo.classList.add("superclase");
+       mensaje.classList.add("segundaSuperClase"); 
+       subtitulo.innerText = `¡ERES UN MAESTRO POKEMON!`
+       iconoEstrella.src = "estrella.png";
+       subtitulo.appendChild(iconoEstrella);
+    
+     
+    break;
 }
 
-else if(entrada.value.toLowerCase().trim() == "" ){
-    sumaPuntos += 0;
-    
-}
-     
- puntaje.innerText = `Puntos: ${sumaPuntos } pts`;
+ 
+ 
  
 
 })
