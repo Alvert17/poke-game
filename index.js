f725c6a84dbb628a1544b13e54549977bf762dd8
Alvert <alvertfabianaponte26@gmail.com>
2025-08-11 13:44:54 -0300

    responsive

--- index.js
+++ index.js
@@ -9,17 +9,17 @@ const sonido = document.getElementById("miSonido");
 const puntardos = document.getElementById("puntaje");
 
 const iconoMankey = document.createElement("img");
-iconoMankey.classList.add("claseMono");
+iconoMankey.classList.add("iconos");
 
 const iconoPika = document.createElement("img");
-iconoPika.classList.add("clasePika");
+iconoPika.classList.add("iconos");
 
 const iconoJiggly = document.createElement("img");
-iconoJiggly.classList.add("clasePuff");
+iconoJiggly.classList.add("iconos");
 
 
 const iconoEstrella = document.createElement("img");
-iconoEstrella.classList.add("estrellita");
+iconoEstrella.classList.add("iconos");
 
 let valorObjetoDeRepuesta  ;
 
@@ -85,14 +85,11 @@ const array = [
 
 botonJuego.addEventListener("click", () => {
     
-     subtitulo.style.textShadow ="5px 4px 3px rgba(100, 100, 100, 1)"
+     subtitulo.style.textShadow ="none ";
     subtitulo.style.color = "#000"; 
     imagenarda.style.filter = "brightness(0) ";
-    subtitulo.classList.remove("superclase");
-    mensaje.classList.remove("mensaje");
-    mensaje.style.fontSize = "";
-    mensaje.style.paddingRight = "";
-    subtitulo.style.textShadow ="";
+    subtitulo.classList.remove("segundaSuperClase"); 
+    
     
     entrada.value = "";
     
@@ -137,7 +134,7 @@ if(entrada.value.toLowerCase().trim() === valorObjetoDeRepuesta.name){
     imagenarda.style.filter = "none";
     subtitulo.innerText = `¡Felicidades es ${valorObjetoDeRepuesta.name}!`;
     subtitulo.style.color = "rgba(87, 201, 21, 1)"
-    subtitulo.style.textShadow ="6px 4px 0px rgba(0, 0, 0, 1)";
+    subtitulo.style.textShadow ="5px 4px 2px rgba(0, 0, 0, 1)";
     sumaPuntos += 1;
     puntaje.innerText = `Puntos: ${sumaPuntos } pts`;   }
     
@@ -145,44 +142,36 @@ if(entrada.value.toLowerCase().trim() === valorObjetoDeRepuesta.name){
  
 switch(sumaPuntos){
     case 5:
-        subtitulo.innerText = "5 puntos? nada mal.."
+        subtitulo.innerText = "5 puntos? no lo haces nada mal.."
         subtitulo.style.color = "rgba(199, 70, 10, 1)"
-        subtitulo.classList.add("superclase");
-       subtitulo.style.textShadow ="5px 4px 0px rgba(0, 0, 0, 1)";
-       iconoMankey.src = "mankey.png";
-       mensaje.style.fontSize = "40px";
+        subtitulo.classList.add("segundaSuperClase"); 
+        iconoMankey.src = "mankey.png";
        subtitulo.appendChild(iconoMankey);
         
     break;
 
-    case 10:
+    case 12:
         subtitulo.innerText = "¡VAYA!si que sabes de pokemons"
         subtitulo.style.color = "rgba(218, 96, 163, 1)"
-        subtitulo.classList.add("superclase");
+        subtitulo.classList.add("segundaSuperClase"); 
         iconoJiggly.src = "jiggly.png";
-       subtitulo.appendChild(iconoJiggly);
-       mensaje.style.paddingRight = "20%";
-       subtitulo.style.textShadow ="5px 4px 0px rgba(0, 0, 0, 1)";
+        subtitulo.appendChild(iconoJiggly);
        break;
       
 
     case 15:
-        subtitulo.innerText = "15pts? sabes mucho de pokemons"
+        subtitulo.innerText = "10pts? sabes mucho de pokemon"
         subtitulo.style.color = "rgba(218, 126, 7, 1)"
-        subtitulo.classList.add("superclase");
+        subtitulo.classList.add("segundaSuperClase"); 
         iconoPika.src = "pika.png";
         subtitulo.appendChild(iconoPika);
-        mensaje.style.paddingRight = "20%";
-        mensaje.style.fontSize = "32px";
-        subtitulo.style.textShadow ="5px 4px 0px rgba(0, 0, 0, 1)";
     break;
 
 
-    case 20:
+    case 18:
        subtitulo.style.color ="rgba(245, 181, 6, 1) "; 
-       subtitulo.classList.add("superclase");
-       mensaje.classList.add("segundaSuperClase"); 
-       subtitulo.innerText = `¡ERES UN MAESTRO POKEMON!`
+       subtitulo.classList.add("segundaSuperClase"); 
+       subtitulo.innerText = `¡ oyee ERES UN MAESTRO POKEMON!`
        iconoEstrella.src = "estrella.png";
        subtitulo.appendChild(iconoEstrella);
     
